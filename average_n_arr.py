1025b5f24d22aa24160aac9756fb917129cd558a
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2023-01-20 12:35:09 +0000

    Solve without using indexes

--- average_n_arr.py
+++ average_n_arr.py
@@ -1,8 +1,16 @@
 # Oldie method
+# def find_avg(arr):
+#     total = 0
+#     for i in range(len(arr)):
+#         total += arr[i]
+#     return total / len(arr)
+
+
+# Without using indexes
 def find_avg(arr):
     total = 0
-    for i in range(len(arr)):
-        total += arr[i]
+    for i in arr:
+        total += i
     return total / len(arr)
 
 
