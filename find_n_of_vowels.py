ef00e2ef4cb8dadd5ef63f304dfa1e1da57cb78e
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2023-02-07 23:53:47 +0000

    add n of vowels

---
+++ find_n_of_vowels.py
@@ -0,0 +1,8 @@
+
+
+
+print(count_vowels("Hello, world!")) # should print 3
+print(count_vowels("This is a test")) # should print 4
+print(count_vowels("abcdefghijklmnopqrstuvwxyz")) # should print 5
+print(count_vowels("AEIOU")) # should print 5
+print(count_vowels("")) # should print 0
