5a64cca9fba00544b3ec7643e9952ee7250bc03a
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2023-02-08 12:48:48 +0000

    Solve n of vowels

--- find_n_of_vowels.py
+++ find_n_of_vowels.py
@@ -1,5 +1,11 @@
-
-
+def count_vowels(str):
+    counter = 0
+    lower_str = str.lower()
+    for i in range(len(lower_str) - 1):
+        char = lower_str[i]
+        if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
+            counter += 1
+    return counter
 
 print(count_vowels("Hello, world!")) # should print 3
 print(count_vowels("This is a test")) # should print 4
